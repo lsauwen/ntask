83034a9ce0e0fdcbbd3c1953fe8a90af3aa2cd2c
lsauwen <leandroassis@gmail.com>
2016-03-02 18:54:02 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import consign from "consign";
+const PORT = 3000;
+
+const app = express();
+
+consign()
+	.include("db.js")
+	.then("models")
+	.then("libs/middlewares.js")
+	.then("routes")
+	.then("libs/boot.js")
+	.into(app);
+
+
+// app.listen(PORT, () => console.log('NTask API - porta '+PORT));
