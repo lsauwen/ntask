ff2285f040ebda85d8f547df425e4a7df4a5aa31
Leandro Sauwen <y7dt@petrobras.com.br>
2016-03-11 14:19:27 -0300

    mapeamento dos models

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import express from "express";
 import consign from "consign";
-const PORT = 3000;
 
 const app = express();
 
 consign()
+	.include("libs/config.js")
 	.include("db.js")
-	.then("models")
 	.then("libs/middlewares.js")
 	.then("routes")
 	.then("libs/boot.js")
